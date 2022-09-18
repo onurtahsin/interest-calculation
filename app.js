60059f7c53f4509bbc0a3c31ce6266f025c6abce
onur <kizilayonurtahsin@gmaik.com>
2022-09-19 00:01:41 +0300

    v1

---
+++ app.js
@@ -0,0 +1,55 @@
+const select = document.getElementById("bank");
+const vade = document.getElementById("vade");
+const tutar = document.getElementById("tutar");
+const button = document.getElementById("btn");
+
+let oran = 0;
+let taksit = 0;
+
+button.addEventListener("click", () => {
+  if (select.value === "Seçiniz:" || vade.value === "" || tutar.value === "") {
+    alert("Tüm Alanları Doldurmalısınız");
+  } else {
+    if (select.value === "Konut") {
+      oran = 2.04;
+    } else if (select.value === "Araç") {
+      oran = 2.08;
+    } else if (select.value === "İhtiyaç") {
+      oran = 1.99;
+    }
+  }
+  const faizOranı = oran / 100;
+  taksit =
+    (tutar.value * (faizOranı * (1 + faizOranı) ** vade.value)) /
+    ((1 + faizOranı) ** vade.value - 1);
+  sonuc();
+});
+
+const sonuc = () => {
+  const sonuc = document.querySelector(".sonuc");
+  sonuc.innerHTML = `
+        <h2>Kredi Bilgileri</h2>
+        <table class="table">
+          <tbody>
+            <tr>
+              <th scope="row">Kredi Miktarı</th>
+              <td>${tutar.value}</td>
+              <th>Kredi Tipi</th>
+              <td>${select.value}</td>
+            </tr>
+            <tr>
+              <th scope="row">Vade</th>
+              <td>${vade.value}</td>
+              <th>Faiz Oranı</th>
+              <td>${oran}</td>
+            </tr>
+            <tr>
+              <th scope="row">Toplam Tutar</th>
+              <td>${(taksit * vade.value).toFixed(2)}</td>
+              <th>Taksit Ödemesi</th>
+              <td>${taksit.toFixed(2)}</td>
+            </tr>
+          </tbody>
+        </table>
+        `;
+};
